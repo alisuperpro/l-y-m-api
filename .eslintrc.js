6591408d0502bc62aa1bd2e19bdeac524749e24c
alisuperpro <alisolorzanove@gmail.com>
2025-06-03 17:50:54 -0400

    firts commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "standard-with-typescript",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/space-before-function-paren": "off",
+    "@typescript-eslint/no-extraneous-class": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/unbound-method": "off",
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import app from '../api/index'
+const PORT = process.env.PORT ?? 3500
+
+app.listen(PORT, () => {
+    console.log(`Server run on port: ${PORT}`)
+})
