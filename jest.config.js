42389361dd1e3e499f120c4fa99f4028eee89d61
alisuperpro <alisolorzanove@gmail.com>
2025-06-04 19:46:30 -0400

    update jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: true,
+    detectOpenHandles: true,
+    testTimeout: 15000,
+    forceExit: true,
 }
