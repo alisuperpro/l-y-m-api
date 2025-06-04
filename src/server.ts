09aad1f05bc92ec86c3c4a0342849e2e5aba346d
alisuperpro <alisolorzanove@gmail.com>
2025-06-04 19:46:09 -0400

    add node_env and refact to test

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,6 @@
-import express from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
-import dotenv from 'dotenv'
-import { employeeRouter } from './routes/employee'
-import { clientRouter } from './routes/client'
-import { debtRouter } from './routes/debt'
+import app from '../api/index'
+const PORT = process.env.PORT ?? 3500
 
-const app = express()
-
-dotenv.config()
-const corsOptions = {
-    origin: ['*'],
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.use(cors(corsOptions))
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-app.get('/', async (_: any, res: { send: (arg0: string) => void }) => {
-    res.send('hello world')
+app.listen(PORT, () => {
+    console.log(`Server run on port: ${PORT}`)
 })
-
-app.use('/employee', employeeRouter)
-app.use('/client', clientRouter)
-app.use('/debt', debtRouter)
-
-export default app
