c86249657a51eca56ecf557e80ecb70757a4250d
alisuperpro <alisolorzanove@gmail.com>
2025-06-05 11:55:49 -0400

    add new endpoint

--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -4,6 +4,11 @@ import { EmployeeController } from '../controller/employee'
 export const employeeRouter = express.Router()
 
 employeeRouter.get('/', EmployeeController.getAll)
+employeeRouter.get('/username', EmployeeController.findByUsername)
 employeeRouter.get('/:id', EmployeeController.findEmployeeById)
 
 employeeRouter.post('/add', EmployeeController.add)
+
+if (process.env.NODE_ENV === 'test') {
+    employeeRouter.delete('/', EmployeeController.delete)
+}
