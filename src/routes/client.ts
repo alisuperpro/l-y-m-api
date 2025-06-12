d241b82f7ac4208077a1a195d7e40db4869985fa
alisuperpro <alisolorzanove@gmail.com>
2025-06-12 16:11:07 -0400

    añadir una nueva ruta en el enrutador de clientes para buscar clientes por nombre de usuario, mejorando la funcionalidad de la API.

--- src/routes/client.ts
+++ src/routes/client.ts
@@ -4,6 +4,7 @@ import { ClientController } from '../controller/client'
 export const clientRouter = express.Router()
 
 clientRouter.get('/', ClientController.getAll)
+clientRouter.get('/username', ClientController.findByUsername)
 clientRouter.get('/:id', ClientController.findClientById)
 
 clientRouter.post('/add', ClientController.add)
