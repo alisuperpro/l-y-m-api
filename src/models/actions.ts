5b9ff4417484f75ce076b32e9613e95771dbbb3a
alisuperpro <alisolorzanove@gmail.com>
2025-06-17 17:39:29 -0400

    add actions models

---
+++ src/models/actions.ts
@@ -0,0 +1,56 @@
+import { db } from '../db/db'
+import { randomUUID } from 'node:crypto'
+
+export class ActionsModel {
+    static tableName = 'actions'
+
+    static async add({
+        name,
+        description,
+    }: {
+        name: string
+        description: string
+    }) {
+        const id = randomUUID()
+        try {
+            await db.execute({
+                sql: `INSERT INTO ${this.tableName} (id, name, description) VALUES (?, ?, ?)`,
+                args: [id, name, description],
+            })
+
+            const result = await db.execute({
+                sql: `SELECT * FROM ${this.tableName} WHERE id = ?`,
+                args: [id],
+            })
+
+            return [undefined, result.rows[0]]
+        } catch (error) {
+            return [error]
+        }
+    }
+
+    static async getById({ id }: { id: string }) {
+        try {
+            const result = await db.execute({
+                sql: `SELECT * FROM ${this.tableName} WHERE id = ?`,
+                args: [id],
+            })
+
+            return [undefined, result.rows[0]]
+        } catch (error) {
+            return [error]
+        }
+    }
+
+    static async getAll() {
+        try {
+            const result = await db.execute({
+                sql: `SELECT * FROM ${this.tableName}`,
+            })
+
+            return [undefined, result.rows]
+        } catch (error) {
+            return [error]
+        }
+    }
+}
