52c1a4b184f450b7d59f228bd7a087ebe8fa54eb
alisuperpro <alisolorzanove@gmail.com>
2025-06-22 18:28:33 -0400

    Actualizar rutas de empleados para incluir verificación de token y permisos de ruta en todas las operaciones, mejorando la seguridad y el control de acceso.

--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -1,18 +1,25 @@
 import express from 'express'
 import { EmployeeController } from '../controller/employee'
 import { verifyToken } from '../middleware/verifyToken'
+import { authoEmployee } from '../middleware/authEmployee'
+import { setRoutePermission } from '../middleware/loadPermission'
 
 export const employeeRouter = express.Router()
-employeeRouter.use(verifyToken)
 
-employeeRouter.get('/', EmployeeController.getAll)
-employeeRouter.get('/username', EmployeeController.findByUsername)
-employeeRouter.get('/:id', EmployeeController.findEmployeeById)
+employeeRouter.get(
+    '/',
+    verifyToken,
+    setRoutePermission.loadRoutePermission,
+    authoEmployee,
+    EmployeeController.getAll
+)
+employeeRouter.get('/username', verifyToken, EmployeeController.findByUsername)
+employeeRouter.get('/:id', verifyToken, EmployeeController.findEmployeeById)
 
-employeeRouter.post('/add', EmployeeController.add)
+employeeRouter.post('/add', verifyToken, EmployeeController.add)
 employeeRouter.post('/login', EmployeeController.login)
-employeeRouter.post('/logout', EmployeeController.logout)
+employeeRouter.post('/logout', verifyToken, EmployeeController.logout)
 
 if (process.env.NODE_ENV === 'test') {
-    employeeRouter.delete('/', EmployeeController.delete)
+    employeeRouter.delete('/', verifyToken, EmployeeController.delete)
 }
