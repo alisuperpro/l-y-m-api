9130690d87912c777fbe2e3bbdb6795a45c0b29e
alisuperpro <alisolorzanove@gmail.com>
2025-06-22 18:27:26 -0400

    Agregar middleware para cargar permisos de ruta basado en la solicitud del usuario.

---
+++ src/middleware/loadPermission.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express'
+import { RoutePermissionMapModel } from '../models/routePermissionMap'
+
+export class setRoutePermission {
+    static async loadRoutePermission(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        const path = `${req.baseUrl}${req.path}`
+        const [error, result] = await RoutePermissionMapModel.getByRoutePath({
+            routePath: path,
+        })
+        if (error) {
+            res.status(500).json({
+                error: 'Error al obtener los permisos de la ruta',
+            })
+            return
+        }
+
+        if (!result) {
+            res.status(404).json({ error: 'Ruta no encontrada' })
+            return
+        }
+
+        //@ts-ignore
+        req.routePermission = result
+        next()
+    }
+}
